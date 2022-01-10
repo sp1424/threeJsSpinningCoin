ae746b3f7e9cc6e4cef1a363bee9d3dde8cc1b62
sp1424 <sebastianpira1999@outlook.com>
2022-01-10 20:48:35 +0000

    refactred 3js boiletplate

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+import * as THREE from './node_modules/three/build/three.module.js';
 
-app.use(express.static('public'));
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-app.get('/', function(req, res) {
-    res.render('index')
-});
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
 
-app.listen(port, function() {
-  console.log(`Example app listening on port ${port}!`)
-});
+camera.position.z = 5;
+function animate() {
+	requestAnimationFrame( animate );
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+	renderer.render( scene, camera );
+}
+animate();
