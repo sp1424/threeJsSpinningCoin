1de23d8db5954b03bd11e8745dd05ad6fbda902c
sp1424 <sebastianpira1999@outlook.com>
2022-01-10 22:29:23 +0000

    changed rotating cube to arrow

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 import * as THREE from './node_modules/three/build/three.module.js';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
 
-camera.position.z = 5;
-function animate() {
-	requestAnimationFrame( animate );
+const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
+camera.position.set( 0, 0, 100 );
+camera.lookAt( 0, 0, 0 );
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+const scene = new THREE.Scene();
+const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
+const points = [];
+points.push( new THREE.Vector3( - 10, 0, 0 ) );
+points.push( new THREE.Vector3( 0, 10, 0 ) );
+points.push( new THREE.Vector3( 10, 0, 0 ) );
 
-	renderer.render( scene, camera );
-}
-animate();
+const geometry = new THREE.BufferGeometry().setFromPoints( points );
+const line = new THREE.Line( geometry, material );
+scene.add( line );
+renderer.render( scene, camera );
