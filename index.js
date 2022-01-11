f56d09bd10a3d0140144befc806b1f00d4fdabd1
sp1424 <sebastianpira1999@outlook.com>
2022-01-11 23:10:22 +0000

    starting coin rotation with mousemove and click

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.121.1/build/three.module.js';
 import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/loaders/GLTFLoader.js';
+import { DragControls } from "https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/controls/DragControls.js";
 
 const renderer = new THREE.WebGLRenderer();
 renderer.outputEncoding = THREE.sRGBEncoding;
@@ -19,11 +20,14 @@ const dirLight = new THREE.DirectionalLight( 0xffffff, 0.8 );
 dirLight.position.set( 0, 0, 10 );
 scene.add( dirLight )
 
+let rotate = true;
+let box = undefined;
+
 function animate() {
 	requestAnimationFrame( animate );
 	renderer.render( scene, camera );
 
-    if(gltfobj !== undefined){
+    if(gltfobj !== undefined && rotate){
         gltfobj.rotation.y +=0.01;
     }
 }
@@ -32,6 +36,7 @@ loader.load('./scenes/ethereum/scene.gltf',
     (gltf) => {
         gltfobj = gltf.scene;
         scene.add(gltfobj);
+        box = new THREE.Box3().setFromObject(gltfobj);
         renderer.render( scene, camera );
     },
 
@@ -40,4 +45,17 @@ loader.load('./scenes/ethereum/scene.gltf',
     }
 );
 
-animate();
+let mousedown = false;
+
+document.body.onmousedown = function() { 
+    mousedown = true;
+  }
+  document.body.onmouseup = function() {
+    mousedown = false;
+}
+
+document.addEventListener('mousemove', (p) => {
+    if(mousedown){
+        animate();
+    }
+}, false);
