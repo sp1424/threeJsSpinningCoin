14022fdc52c5dae4a6c72644c21f331f9e61bb63
sp1424 <sebastianpira1999@outlook.com>
2022-01-11 21:02:24 +0000

    gltf rendering with rotation + coin

--- index.js
+++ index.js
@@ -6,24 +6,38 @@ renderer.outputEncoding = THREE.sRGBEncoding;
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.set( 0, 1000, 0 );
+camera.position.set( 0, 0, 10 );
 camera.lookAt( 0, 0, 0 );
 const scene = new THREE.Scene();
 const loader = new GLTFLoader();
 const hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444, 0.4 );
-hemiLight.position.set( 0, 20, 0 );
+hemiLight.position.set( 0, 20, 10 );
 scene.add( hemiLight );
+let gltfobj = undefined;
 
 const dirLight = new THREE.DirectionalLight( 0xffffff, 0.8 );
-dirLight.position.set( - 3, 10, - 10 );
+dirLight.position.set( 0, 0, 10 );
 scene.add( dirLight )
 
-loader.load('./2CylinderEngine.glb', 
+function animate() {
+	requestAnimationFrame( animate );
+	renderer.render( scene, camera );
+
+    if(gltfobj !== undefined){
+        gltfobj.rotation.y +=0.01;
+    }
+}
+
+loader.load('./scenes/ethereum/scene.gltf', 
     (gltf) => {
-        scene.add(gltf.scene);
+        gltfobj = gltf.scene;
+        scene.add(gltfobj);
         renderer.render( scene, camera );
     },
+
     undefined, (error) => {
         console.log(error);
     }
 );
+
+animate();
